4526b1e04b241f42da1636f72b627e85fdb15845
Pedro Afonso <pedfonso10@gmail.com>
2026-03-19 12:39:20 -0300

    integracao cadastro e login concluidas

--- projetofaq/projetofaq/settings.py
+++ projetofaq/projetofaq/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'appfaq'
+    'appfaq',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'projetofaq.urls'
 
